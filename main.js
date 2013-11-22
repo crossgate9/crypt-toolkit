f4397bd3a264ea50c45a252e6db1bf6a71803bee
crossgate9 <crossgate9@gmail.com>
2013-11-22 11:50:21 +0800

    [bug] not consider the value should contain in the candidate set

--- main.js
+++ main.js
@@ -22,14 +22,58 @@ var data = [
 ];
 
 var c;
+var used;
 
 var category = function(v) {
     var w = v[0] * 8 + v[1] * 4 + v[2] * 2 + v[3],
         y = v[4] * 8 + v[5] * 4 + v[6] * 2 + v[7],
         z = v[8] * 8 + v[9] * 4 + v[10] * 2 + v[11];
+    var vv = w.toString() + ',' + y.toString() + ',' + z.toString();
+    if (_.indexOf(used, vv) !== -1) {
+        return;
+    }
+    used.push(vv);
 
-    var yc = (y === 6 || y === 11),
-        zc = (z === 6 || z === 11);
+    var yc, zc;
+    var v1, v2;
+
+    switch (w) {
+        case 2:
+            v1 = 6; v2 = 11;
+            if (y === 0 || y === 1 || y === 7 || y === 10 || y === 12 || y === 13) return;
+            if (z === 0 || z === 1 || z === 7 || z === 10 || z === 12 || z === 13) return;
+            break;
+        case 4:
+            v1 = 9; v2 = 14;
+            if (y === 0 || y === 1 || y === 6 || y === 7 || y === 8 || y === 15) return;
+            if (z === 0 || z === 1 || z === 6 || z === 7 || z === 8 || z === 15) return;
+            break;
+        case 6:
+            v1 = 1; v2 = 2;
+            if (y === 3 || y === 4 || y === 5 || y === 6 || y === 7 || y === 0) return;
+            if (z === 3 || z === 4 || z === 5 || z === 6 || z === 7 || z === 0) return;
+            break;
+        case 8:
+            v1 = 11; v2 = 13;
+            if (y === 0 || y === 2 || y === 4 || y === 6 || y === 9 || y === 15) return;
+            if (z === 0 || z === 2 || z === 4 || z === 6 || z === 9 || z === 15) return;
+            break;
+        case 13:
+            v1 = 8; v2 = 12;
+            if (y === 0 || y === 1 || y === 4 || y === 5 || y === 9 || y === 13) return;
+            if (z === 0 || z === 1 || z === 4 || z === 5 || z === 9 || z === 13) return;
+            break;
+        case 14:
+            v1 = 4; v2 = 12;
+            if (y === 0 || y === 2 || y === 6 || y === 8 || y === 10 || y === 14) return;
+            if (z === 0 || z === 2 || z === 6 || z === 8 || z === 10 || z === 14) return;
+            break;
+        default:
+            return;
+    }
+
+    yc = (y === v1 || y === v2);
+    zc = (z === v1 || z === v2);
 
     if (yc === true && zc === true) {
         c[0] ++;
@@ -79,6 +123,7 @@ var output = function(d, c) {
         process.stdout.write(']');
     }
     process.stdout.write(' - (' + c[0] + ',' + c[1] + ',' + c[2] + ')');
+    process.stdout.write(' - ' + (c[0]+c[1]+c[2]));
     process.stdout.write('\n');
 };
 
@@ -89,6 +134,7 @@ for (i = 0; i < perm.length; i++) {
     d = _.difference(data, d);
     a = [];
     c = [0, 0, 0];
+    used = [];
     sub(d, d.length - 1);
     output(d, c);
 }
